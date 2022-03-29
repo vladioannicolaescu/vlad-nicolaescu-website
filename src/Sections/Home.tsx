db5b96ffe905831dd21621dfbad5c533b56d703f
vlad <vn119@ic.ac.uk>
2022-03-29 15:53:37 +0100

    Home page implementation

--- src/Sections/Home.tsx
+++ src/Sections/Home.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import './home.css';
-import {LandingPicture, LandingPictureCover} from '../components/Sections/Home/Elements';
+import {LandingPicture, LandingPictureCover, Motto, TitleGroup, TitleLine, DesignLine, Title, Subtitle} from '../components/Sections/Home/Elements';
+import Typewriter from "typewriter-effect";
 
 function HomeSection() {
     return (
         <div id="home">
             <LandingPicture>
                 <LandingPictureCover>
-
+                    <Motto>
+                        Dream it. Believe it. Do it.
+                    </Motto>
+                    <TitleGroup>
+                        <TitleLine>
+                            <DesignLine/>
+                            <Title>
+                                <Typewriter
+                                    onInit={(typewriter)=> {
+                                        typewriter
+                                        .typeString("Hello, I'm Vlad ... ")
+                                        .pauseFor(1000)
+                                        .deleteAll()
+                                        .typeString("Vlad Nicolaescu")
+                                        .start();
+                                    }}/>
+                            </Title>
+                            <DesignLine/>
+                        </TitleLine>
+                        <Subtitle>
+                            Software Engineering Intern - Google
+                        </Subtitle>
+                        <Subtitle>
+                            Final Year MEng Student - Imperial College London
+                        </Subtitle>
+                        <img src="../images/image-removebg-preview.png" alt="" />
+                    </TitleGroup>
                 </LandingPictureCover>
             </LandingPicture>
         </div>
--- src/components/Sections/Home/Elements.tsx
+++ src/components/Sections/Home/Elements.tsx
@@ -13,4 +13,74 @@ export const LandingPictureCover = styled.div`
     background: rgb(0, 0, 0, 0.75);
     height: 100%;
     width: 100%;
-`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding-top: 150px;
+    padding-bottom: 150px;
+`;
+
+export const Motto = styled.h1`
+    font-family: 'Courier New';
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    font-size: 2.5rem;
+    font-weight: 600;
+`;
+
+export const TitleGroup = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 0 5vw;
+
+    img {
+        height: 300px;
+    }
+
+`;
+
+export const TitleLine = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+`;
+
+export const DesignLine = styled.div`
+    content: '';
+    height: 2.5px;
+    width: 100%;
+    background: #fff;
+    margin: auto;
+`;
+
+export const Title = styled.h1`
+    font-family: 'Courier New';
+    color: white;
+    display: inline-block;
+    text-align: center;
+    font-size: 3rem;
+    font-weight: 500;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    padding: 0 50px;
+
+    @media screen and (min-width: 800px) {
+        white-space: nowrap;
+    }
+`;
+
+export const Subtitle = styled.h2`
+    font-family: 'Geneva';
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    font-weight: 500;
+`;
