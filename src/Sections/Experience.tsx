0f690aa4ebd5e94b9711de2c6511cf3cec4ed23d
vlad <vn119@ic.ac.uk>
2022-03-30 21:49:32 +0100

    Experience implemented

--- src/Sections/Experience.tsx
+++ src/Sections/Experience.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './experience.css';
-import {} from '../components/Sections/Portfolio/Elements';
-import {Container, TitleLine, DesignLine, Title, Subtitle} from '../components/utils/Elements';
+import {Container, Subtitle, HorizontalLine, Side, CompanyImage, CompanyInfo, Position, Period, Location} from '../components/Sections/Experience/Elements';
+import {TitleLine, DesignLine, Title} from '../components/utils/Elements';
 
 function ExperienceSection() {
     return (
@@ -16,8 +16,50 @@ function ExperienceSection() {
                 </TitleLine>
 
                 <Subtitle>Skills & Tools</Subtitle>
+
                 <Subtitle>Companies</Subtitle>
+                <Container>
+                    <HorizontalLine>
+                        <Side>
+                            <CompanyImage src="../images/GoogleLogo.png"/>
+                        </Side>
+                        <Side>
+                            <CompanyInfo>
+                                <Position>Software Engineer Intern</Position>
+                                <Period>May 2022 - September 2022</Period>
+                                <Location>Zurich, Switzerland</Location>
+                            </CompanyInfo>
+                        </Side>
+                    </HorizontalLine>
+                </Container>
+
                 <Subtitle>Education</Subtitle>
+                <Container>
+                    <HorizontalLine>
+                        <Side>
+                            <CompanyImage src="../images/ImperialCollegeLogo.png"/>
+                        </Side>
+                        <Side>
+                            <CompanyInfo>
+                                <Position>Master Of Engineering in Computing</Position>
+                                <Period>September 2019 - July 2023</Period>
+                                <Location>London, UK</Location>
+                            </CompanyInfo>
+                        </Side>
+                    </HorizontalLine>
+                    <HorizontalLine>
+                        <Side>
+                            <CompanyImage src="../images/SavaLogo.png"/>
+                        </Side>
+                        <Side>
+                            <CompanyInfo>
+                                <Position>High School Diploma</Position>
+                                <Period>September 2015 - July 2019</Period>
+                                <Location>Bucharest, Romania</Location>
+                            </CompanyInfo>
+                        </Side>
+                    </HorizontalLine>
+                </Container>
 
             </Container>
         </div>
--- src/components/Sections/Experience/Elements.tsx
+++ src/components/Sections/Experience/Elements.tsx
@@ -1,3 +1,79 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+    padding: 25px 10%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: fit-content;
+`;
 
+export const HorizontalLine = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    border: 2px solid #555;
+    padding: 30px 0;
+    margin: 25px 0;
+    background: #eeeeee;
+    border-radius: 10px;
+    box-shadow: 5px 5px rgba(0, 0, 0, 0.2);
+
+    @media screen and (max-width: 1000px) {
+        flex-direction: column;
+    }
+`;
+
+export const Side = styled.div`
+    width: 40vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px auto;
+
+    @media screen and (max-width: 1000px) {
+        width: 80vw;
+    }
+`;
+
+export const Subtitle = styled.h2`
+    color: #000;
+    display: inline-block;
+    text-align: center;
+    font-size: 2.2rem;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    padding: 0 50px;
+
+    @media screen and (min-width: 800px) {
+        white-space: nowrap;
+    }
+`;
+
+export const CompanyImage = styled.img`
+    height: 100px;
+    margin: 10px auto;
+`;
+
+export const CompanyInfo = styled.div`
+    height: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 10px auto;
+`;
+
+export const Position = styled.h1`
+    font-weight: 600;
+`;
+
+export const Period = styled.h2`
+    font-weight: 500;
+`;
+
+export const Location = styled.h3`
+    font-weight: 500;
+`;
